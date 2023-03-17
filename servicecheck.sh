63415d6b1cce290cff1c1b5df9c39fbfc1595408
Arpita Patnaik <32553590+arpita497@users.noreply.github.com>
2023-03-17 16:50:33 +0530

    Update servicecheck.sh

--- servicecheck.sh
+++ servicecheck.sh
@@ -7,3 +7,11 @@ then
 else
   sudo kubectl apply -f ./web_service.yaml
 fi
+
+sudo kubectl get svc mongo
+if [ $(echo $?) == "0" ]
+then
+  echo "service already exists"
+else
+  sudo kubectl apply -f ./mongo_service.yaml
+fi
